bae6077fd059fb5b320bedd27c6ce15bc7cf0307
Frits Hoogland <fhoogland@yugabyte.com>
2023-03-10 15:58:36 +0100

    Rust cdrs example

--- cdrs/examples/cloud.rs
+++ cdrs/examples/cloud.rs
@@ -11,14 +11,15 @@ use openssl::ssl::{SslConnector, SslMethod};
 use std::env;
 
 fn main() {
+    // reading the variables from the environment variables
     let node_address: String = env::var("NODE_ADDRESS").expect("NODE_ADDRESS must be set");
     let ca_certificate: String = env::var("CA_CERTIFICATE").expect("CA_CERTIFICATE must be set");
     let username: String = env::var("USER_NAME").expect("USER_NAME must be set");
     let password: String = env::var("PASSWORD").expect("PASSWORD must be set");
 
     let authenticator = StaticPasswordAuthenticator::new(username, password);
-    let mut ssl_build = SslConnector::builder(SslMethod::tls()).unwrap();
-    ssl_build.set_ca_file(ca_certificate).unwrap();
+    let mut ssl_build = SslConnector::builder(SslMethod::tls()).expect("Error building ssl connector");
+    ssl_build.set_ca_file(ca_certificate).expect("Error reading CA certificate");
     let ssl_connector = ssl_build.build();
 
     let node = NodeSslConfigBuilder::new(node_address.as_str(), authenticator, ssl_connector).build();
@@ -26,11 +27,11 @@ fn main() {
     let session = new_ssl(&cluster_config, RoundRobin::new()).unwrap();
 
     let query= "SELECT keyspace_name FROM system_schema.keyspaces";
-    let rows = session.query(query).unwrap()
+    let rows = session.query(query).expect("Error executing query")
         .get_body().unwrap()
         .into_rows().unwrap();
     for row in rows.iter() {
-        let col: String = row.get_r_by_name("keyspace_name").unwrap();
-        println!("ks name = {}", col);
+        let col: String = row.get_r_by_name("keyspace_name").expect("Error getting row for column using get_r_by_name()");
+        println!("keyspace_name = {}", col);
     }
 }
