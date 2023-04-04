35c40c7e9ddff8b2cbe23026e255a7156d17167a
Frits Hoogland <fhoogland@yugabyte.com>
2023-04-04 16:05:23 +0200

    ssl/cloud connectionpool for sqlx

---
+++ sqlx/examples/connectionpool_cloud.rs
@@ -0,0 +1,23 @@
+use sqlx::{Row, postgres::PgPoolOptions};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let hostname: String = env::var("PGHOST").expect("PGHOST must be set");
+    // The port is set to 5433 (the default YugabyteDB port), set to 5432 for the postgres default.
+    let port: String = env::var("PGPORT").unwrap_or("5433".to_string());
+    // the username is set to yugabyte (the default YugabyteDB username), set to postgres for the postgres default.
+    let username: String = env::var("PGUSER").unwrap_or("admin".to_string());
+    let password: String = env::var("PGPASSWORD").expect("PGPASSWORD must be set");
+    let database: String = env::var("PGDATABASE").unwrap_or("yugabyte".to_string());
+    let ca_certificate_file: String = env::var("PGSSLROOTCERT").expect("PGSSLROOTCERT must be set");
+
+    // create connectionpool
+    let pool = PgPoolOptions::new().min_connections(5).max_connections(5).connect(&format!("postgres://{}:{}@{}:{}/{}?ssl=true&sslmode=verify-full&sslrootcert={}", username, password, hostname, port, database, ca_certificate_file)).await.expect("Error connecting to database");
+
+    let rows = sqlx::query("select table_name from information_schema.tables where table_type = 'BASE TABLE'").fetch_all(&pool).await.expect("Error executing query");
+    for row in rows.iter().map(|row| format!("{}", row.get::<String, _>("table_name"))).collect::<Vec<String>>()
+    {
+        println!("{}", row);
+    }
+}
